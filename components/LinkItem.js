dd94637fd3c9ade19c373e42eb24339e19f47691
Shahadat Hossen <shahadat.hossen.web@gmail.com>
2022-06-27 08:30:51 +0600

    navbar updated

--- components/LinkItem.js
+++ components/LinkItem.js
@@ -4,11 +4,11 @@ import { Link } from "react-awesome-slider/dist/navigation";
 const LinkItem = ({ slug, slugParam, text, className = "" }) => {
   return (
     <Link
-      className={`${slug === slugParam ? "selected bg-gray-700" : null}`}
+      className={`${slug === slugParam ? "selected bg-gray-700 rounded-md" : null}`}
       href={`/${slugParam}`}
     >
       <span
-        className={`text-white block px-3 py-2 rounded-md font-medium ${className}`}
+        className={`text-white hover:bg-gray-800 rounded-md block px-3 py-2 font-medium ${className}`}
       >
         {text}
       </span>
