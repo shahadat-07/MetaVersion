a693d28e836ffb0adb573fc955b29bdabe324ec2
Shahadat Hossen <shahadat.hossen.web@gmail.com>
2022-06-27 22:56:12 +0600

    working on our team section

--- components/OurTeam.js
+++ components/OurTeam.js
@@ -12,11 +12,11 @@ const OurTeam = withNavigationContext(({ fullpage }) => {
     >
       <div className="our-team-bg">
         <PageLayout className="flex items-center ">
-          <div className="flex-1">
+          <div className="basis-1/4">
             <HeadingOne className="mb-3 md:mb-4 lg:mb-8 lg:leading">
               Our team
             </HeadingOne>
-            <div className="text-white mb-6 font-narrow font-medium text-sm lg:text-xl 3xl:text-4xl 3xl:leading-[55px]">
+            <div className="text-white mb-6 font-narrow font-medium text-sm lg:text-lg 3xl:text-4xl 3xl:leading-[55px]">
               <p>
                 - Buy, sell and look for new partners in the NFT marketplace of
                 the future{" "}
@@ -29,8 +29,34 @@ const OurTeam = withNavigationContext(({ fullpage }) => {
               </p>
             </div>
           </div>
-          <div className="flex-3">
-            <h1>lorem </h1>
+          <div className="basis-3/4 pl-8">
+            <div className="relative h-screen"> 
+              <div className="absolute bg-[#5CA1B2] w-[60px] h-[60px] rounded-full top-[32%] left-[8%]" > </div>
+              <div className="absolute bg-[#290A88] w-[100px] h-[100px] rounded-full top-[40%] text-[12px] text-white font-extended font-semibold flex justify-center items-center" >CoinMar<br/>ketCap</div>
+              <div className="absolute bg-[#08A1E3] w-[90px] h-[90px] rounded-full top-[55%] left-[3%]" > </div>
+              <div className="absolute bg-[#C53479] w-[100px] h-[100px] rounded-full" > </div>
+              <div className="absolute bg-[#C53479] w-[100px] h-[100px] rounded-full" > </div>
+              <div className="absolute bg-[#C53479] w-[100px] h-[100px] rounded-full" > </div>
+              <div className="absolute bg-[#C53479] w-[100px] h-[100px] rounded-full" > </div>
+              <div className="absolute bg-[#C53479] w-[100px] h-[100px] rounded-full" > </div>
+              <div className="absolute bg-[#C53479] w-[100px] h-[100px] rounded-full" > </div>
+              <div className="absolute bg-[#C53479] w-[100px] h-[100px] rounded-full" > </div>
+              <div className="absolute bg-[#C53479] w-[100px] h-[100px] rounded-full" > </div>
+              <div className="absolute bg-[#C53479] w-[100px] h-[100px] rounded-full" > </div>
+              <div className="absolute bg-[#C53479] w-[100px] h-[100px] rounded-full" > </div>
+              <div className="absolute bg-[#C53479] w-[100px] h-[100px] rounded-full" > </div>
+              <div className="absolute bg-[#C53479] w-[100px] h-[100px] rounded-full" > </div>
+              <div className="absolute bg-[#C53479] w-[100px] h-[100px] rounded-full" > </div>
+              <div className="absolute bg-[#C53479] w-[100px] h-[100px] rounded-full" > </div>
+              <div className="absolute bg-[#C53479] w-[100px] h-[100px] rounded-full" > </div>
+              <div className="absolute bg-[#C53479] w-[100px] h-[100px] rounded-full" > </div>
+              <div className="absolute bg-[#C53479] w-[100px] h-[100px] rounded-full" > </div>
+              <div className="absolute bg-[#C53479] w-[100px] h-[100px] rounded-full" > </div>
+              <div className="absolute bg-[#C53479] w-[100px] h-[100px] rounded-full" > </div>
+              <div className="absolute bg-[#C53479] w-[100px] h-[100px] rounded-full" > </div>
+              <div className="absolute bg-[#C53479] w-[100px] h-[100px] rounded-full" > </div>
+              <div className="absolute bg-[#C53479] w-[100px] h-[100px] rounded-full" > </div>
+            </div>
           </div>
         </PageLayout>
       </div>
