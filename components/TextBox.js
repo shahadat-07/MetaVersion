448e0648ccaaa3441813ff5694d95b64eebd2c5c
Shahadat Hossen <shahadat.hossen.web@gmail.com>
2022-06-28 22:08:45 +0600

    issue solved

--- components/TextBox.js
+++ components/TextBox.js
@@ -4,12 +4,14 @@ import React from "react";
 const TextBox = ({ src, text }) => {
   return (
     <div className="flex space-x-4 font-narrow">
-       <div className="relative h-6 w-6">
+      <div className="basis-1/6">
         <Image src={src} alt="icon"></Image>
-      </div> 
+      </div>
       {/* <Image src={src} alt="icon"></Image> */}
 
-      <p className="font-medium">{text}</p>
+      <div className="basis-5/6">
+        <p className="font-medium">{text}</p>
+      </div>
     </div>
   );
 };
