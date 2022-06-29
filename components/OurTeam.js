7dabb5b47401095e56e50f4a8b674fb47d1e7210
Shahadat Hossen <shahadat.hossen.web@gmail.com>
2022-06-29 16:02:58 +0600

    numbers removed

--- components/OurTeam.js
+++ components/OurTeam.js
@@ -34,90 +34,90 @@ const OurTeam = withNavigationContext(({ fullpage }) => {
           <div className="basis-3/4 w-full flex-2 lg:px-10 text-center">
             <div className="relative h-screen">
               <ImageCircle
-                text="23 Max CEO & CTO"
+                text="Max CEO & CTO"
                 background="bg-slate-900"
                 position="left-[28%] lg:top-[35%] lg:left-[68%]"
                 topBackground="bg-member-1"
               />
 
               <ImageCircle
-                text="9 Max CEO & CTO"
+                text="Max CEO & CTO"
                 background="bg-slate-700"
                 position="top-[20%] left-[70%] lg:top-[31%] lg:left-[37%] "
                 topBackground="bg-member-2"
               />
 
               <ImageCircle
-                text="10 Max CEO & CTO"
+                text="Max CEO & CTO"
                 background="bg-slate-700"
                 position="top-[19%] left-[2%] lg:top-[60%] lg:left-[16%] "
                 topBackground=" bg-member-3"
               />
 
               <ImageCircle
-                text="23 Max CEO & CTO"
+                text="Max CEO & CTO"
                 background="bg-slate-900"
                 position="top-[37%] left-[35%] lg:top-[65%] lg:left-[45%]"
                 topBackground="bg-member-4"
               />
 
               <TextCircle
-                text="UX 1"
-                position=" left-[30%] top-[14%] lg:top-[18%] lg:left-[40%]"
-                circleSize="w-[60px] h-[60px] lg:top-[w-[75px] lg:h-[75px]"
+                text="UX"
+                position="left-[30%] top-[14%] lg:top-[18%] lg:left-[40%]"
+                circleSize="w-[60px] h-[60px] lg:w-[75px] lg:h-[75px]"
                 background="bg-[#8163A8]"
                 fontSize="text-[14px] lg:text-[20px]"
               />
 
               <TextCircle
-                text="CoinGecko 2"
+                text="CoinGecko"
                 position=" top-[12%] left-[58%] lg:top-[17%] lg:left-[54%]"
                 background="bg-[#C53479]"
               />
 
               <TextCircle
                 text="CoinMar    
-                ketCap 4"
+                ketCap"
                 position="top-[23%] left-[40%] lg:left-[0px] lg:top-[40%]"
                 background="bg-[#290A88]"
               />
 
               <TextCircle
-                text="6 Binance"
+                text="Binance"
                 position="top-[8%] left-[2%] lg:top-[35%] lg:left-[17%]"
                 background="bg-[#F2C94C]"
               />
 
               <TextCircle
                 text="Coinpaprika"
-                position=" top-[35%] lg:top-[46%] lg:left-[27%]"
+                position="top-[35%] lg:top-[46%] lg:left-[27%]"
                 background="bg-[#C53479]"
                 circleSize="w-[100px] h-[100px] lg:w-[130px] lg:h-[130px] 3xl:w-[180px] 3xl:h-[180px]"
               />
 
               <TextCircle
-                text="12 Poo Coin"
+                text="Poo Coin"
                 position="top-[72%] left-[32%]"
                 background="bg-[#664E84]"
                 circleSize="w-[90px] h-[90px] 3xl:w-[140px] 3xl:h-[140px]"
               />
 
               <TextCircle
-                text="Pinksale 15"
+                text="Pinksale"
                 position="top-[2%] left-[64%] lg:top-[45%] lg:left-[53%]"
                 background="bg-[#5CA1B2] "
                 circleSize="w-[75px] h-[75px] 3xl:hidden"
               />
 
               <TextCircle
-                text="20 Pancake Swap"
+                text="Pancake Swap"
                 position=" top-[35%] left-[70%] lg:top-[58%] lg:left-[65%]"
                 circleSize="w-[90px] h-[90px] 3xl:w-[140px] 3xl:h-[140px]"
                 background="bg-[#08A1E3]"
               />
 
               <TextCircle
-                text="21 DEXT"
+                text="DEXT"
                 position=" top-[70%] left-[74%]"
                 background="bg-[#C53479]"
                 circleSize=" w-[90px] h-[90px] 3xl:w-[140px] 3xl:h-[140px]"
@@ -125,70 +125,70 @@ const OurTeam = withNavigationContext(({ fullpage }) => {
               />
 
               <Circle
-                text="3"
-                position=" top-[32%] left-[58%] lg:top-[55%] lg:left-[76%] lg:top-[32%] lg:left-[8%]"
+                text=""
+                position="top-[32%] left-[58%] lg:top-[55%] lg:left-[76%] lg:top-[32%] lg:left-[8%]"
                 background="bg-[#5CA1B2]"
               />
 
               <Circle
-                text="5"
+                text=""
                 position="hidden lg:blcok lg:top-[55%] lg:left-[4%]"
                 background="bg-[#08A1E3]"
               />
 
               <Circle
-                text="7"
+                text=""
                 position="hidden lg:block lg:top-[49%] lg:left-[15%]"
                 background="bg-[#664E84] "
               />
 
               <Circle
-                text="8"
+                text=""
                 position="hidden lg:block lg:top-[30%] lg:left-[29%]"
                 background="bg-[#C53479] "
               />
 
               <Circle
-                text="11"
+                text=""
                 position="top-[73%] left-[8%]"
                 background="bg-[#F2C94C]  "
               />
 
               <Circle
-                text="13"
+                text=""
                 position="hidden lg:block top-[63%] left-[33%]"
                 background="bg-[#553CDF]  "
               />
 
               <Circle
-                text="14"
+                text=""
                 position="hidden lg:block lg:top-[55%] lg:left-[45%]"
                 background="bg-[#8163A8]   "
               />
 
               <Circle
-                text="16"
+                text=""
                 position="top-[8%] left-[85%] lg:top-[32%] lg:left-[57%]"
                 background="bg-[#553CDF]"
                 circleSize="w-[45px] h-[45px] lg:w-[80px] lg:h-[80px] 3xl:w-[130px] 3xl:h-[130px]"
               />
 
               <Circle
-                text="18"
+                text=""
                 position="top-[30%] left-[25%] lg:top-[60%] lg:left-[57%]"
                 background="bg-[#F2C94C]"
                 circleSize="w-[45px] h-[45px]  "
               />
 
               <Circle
-                text="19"
+                text=""
                 position=" hidden lg:block lg:top-[53%] lg:left-[61%]"
                 background="bg-[#C53479]"
                 circleSize="w-[45px] h-[45px] 3xl:w-[90px] 3xl:h-[90px] "
               />
 
               <Circle
-                text="22"
+                text=""
                 position="top-[1%] left-[10%] "
                 background="bg-[#C53479]"
                 circleSize="w-[45px] h-[45px]  "
